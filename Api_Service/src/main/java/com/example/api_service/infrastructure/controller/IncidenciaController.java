b934ffd8c9072cfb4fd25f8ae1440fe322a371eb
sebas2409 <juangonzalezlopez241829@gmail.com>
2023-01-31 10:05:09 +0100

    added entities and repository for ddbb

---
+++ Api_Service/src/main/java/com/example/api_service/infrastructure/controller/IncidenciaController.java
@@ -0,0 +1,7 @@
+package com.example.api_service.infrastructure.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IncidenciaController {
+}
---
+++ Api_Service/src/main/java/com/example/api_service/infrastructure/database/entities/TipoReparacion.java
@@ -0,0 +1,6 @@
+package com.example.api_service.infrastructure.database.entities;
+
+public enum TipoReparacion {
+
+    FISICA, VIRTUAL
+}
